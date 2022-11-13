df6c333c318de290c9f198780b7e275e85fbf659
Dell <Dell@DESKTOP-F4BRUFS>
2022-11-13 21:36:06 +0530

    first commit

---
+++ 589cseb/src/main/java/maven/cseb/App.java
@@ -0,0 +1,13 @@
+package maven.cseb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
